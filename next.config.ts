8fbf992191903c60fcedbaf39650cbc8389f7d24
wy3106714391 <166540495+wy3106714391@users.noreply.github.com>
2026-03-16 16:10:41 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: undefined ,
+  output: undefined,
   transpilePackages: ['mathml2omml', 'pptxgenjs'],
   serverExternalPackages: [],
   experimental: {
